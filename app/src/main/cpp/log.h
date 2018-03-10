f0e9f205c66c52a686bb3ebbc7821cbb07ac8818
742637254@qq.com <dxx13617237755>
2018-03-10 19:46:34 +0800

    jni log功能封装和使用

---
+++ app/src/main/cpp/log.h
@@ -0,0 +1,17 @@
+//
+// Created by Administrator on 2018/3/10.
+//
+
+#ifndef ANDROID_NDK_LOG_H
+#define ANDROID_NDK_LOG_H
+
+#endif //ANDROID_NDK_LOG_H
+
+// log标签
+#define TAG "Jni_Log"
+// 定义info信息
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG,__VA_ARGS__)
+// 定义debug信息
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+// 定义error信息
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)
