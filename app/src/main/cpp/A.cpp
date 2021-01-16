6b93bcf7472be52050d88e0feefe358920c7b040
dawish <742637254@qq.com>
2021-01-16 19:24:39 +0800

    add c++ some codes

---
+++ app/src/main/cpp/A.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Administrator on 2021/1/16.
+//
+
+#include "A.h"
+
+string A::getStr() {
+
+
+    i = 23;
+
+    string str = " default string " + to_string(i);
+
+    return str;
+
+}
---
+++ app/src/main/cpp/A.h
@@ -0,0 +1,17 @@
+//
+// Created by Administrator on 2021/1/16.
+//
+#include <string>
+
+#ifndef ANDROID_NDK_A_H
+#define ANDROID_NDK_A_H
+using namespace std;
+
+class A {
+public:
+    int i;
+    string getStr(void);
+};
+
+
+#endif //ANDROID_NDK_A_H
